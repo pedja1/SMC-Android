b961e028b952a1ee7fff3a08e053c261ffec15ef
Predrag Cokulov <predragcokulov@gmail.com>
2015-08-29 14:02:27 +0200

    af logo on splash

--- core/src/rs/pedjaapps/smc/screen/SplashScreen.java
+++ core/src/rs/pedjaapps/smc/screen/SplashScreen.java
@@ -1,9 +1,13 @@
 package rs.pedjaapps.smc.screen;
 
-import com.badlogic.gdx.*;
-import com.badlogic.gdx.graphics.*;
-import com.badlogic.gdx.graphics.g2d.*;
-import com.badlogic.gdx.graphics.glutils.*;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 import rs.pedjaapps.smc.MaryoGame;
 
@@ -11,13 +15,11 @@ import rs.pedjaapps.smc.MaryoGame;
 public class SplashScreen implements Screen
 {
     private float width, height;
-    private Texture libgdxSplashTexture;
     private Sprite libgdxSplashSprite;
-    private Texture gameSplashTexture;
     private Sprite gameSplashSprite;
+    private Sprite afLogoSprite;
     private SpriteBatch batch;
     private OrthographicCamera camera;
-    ShapeRenderer bgRenderer;
 
     MaryoGame marioGame;
 
@@ -27,7 +29,7 @@ public class SplashScreen implements Screen
     }
 
     private long splashStartTime;
-    private static final long splashDuration = 2 * 1000;//2 sec
+    private static final long splashDuration = 1500;
     int screenToDisplay = 0;
 
     @Override
@@ -40,37 +42,37 @@ public class SplashScreen implements Screen
 
         camera = new OrthographicCamera(1, height/width);
         batch = new SpriteBatch();
-        bgRenderer = new ShapeRenderer();
 
-        libgdxSplashTexture = new Texture(Gdx.files.internal("data/game/logo/libgdx.jpg"));
+        Texture libgdxSplashTexture = new Texture(Gdx.files.internal("data/game/logo/libgdx.jpg"));
         libgdxSplashTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 
-        gameSplashTexture = new Texture(Gdx.files.internal("data/game/logo/smc_big_1.png"));
+        Texture gameSplashTexture = new Texture(Gdx.files.internal("data/game/logo/smc_big_1.png"));
         gameSplashTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
+
+        Texture afLogoTexture = new Texture(Gdx.files.internal("data/game/logo/af_logo.png"));
+        afLogoTexture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 
         libgdxSplashSprite = new Sprite( new TextureRegion(libgdxSplashTexture));
         libgdxSplashSprite.setSize(0.9f, 0.9f * libgdxSplashSprite.getHeight() / libgdxSplashSprite.getWidth());
         libgdxSplashSprite.setOrigin(libgdxSplashSprite.getWidth()/2, libgdxSplashSprite.getHeight()/2);
         libgdxSplashSprite.setPosition(-libgdxSplashSprite.getWidth()/2, -libgdxSplashSprite.getHeight()/2);
 
-
         gameSplashSprite = new Sprite(gameSplashTexture);
         gameSplashSprite.setSize(0.9f, 0.9f * gameSplashSprite.getHeight() / gameSplashSprite.getWidth());
         gameSplashSprite.setOrigin(gameSplashSprite.getWidth()/2, gameSplashSprite.getHeight()/2);
         gameSplashSprite.setPosition(-gameSplashSprite.getWidth()/2, -gameSplashSprite.getHeight()/2);
+
+        afLogoSprite = new Sprite(afLogoTexture);
+        afLogoSprite.setSize(0.9f, 0.9f * afLogoSprite.getHeight() / afLogoSprite.getWidth());
+        afLogoSprite.setOrigin(afLogoSprite.getWidth()/2, afLogoSprite.getHeight()/2);
+        afLogoSprite.setPosition(-afLogoSprite.getWidth()/2, -afLogoSprite.getHeight()/2);
     }
 
     @Override
     public void render(float delta)
     {
-        Gdx.gl.glClearColor(0.1f, 0.1f, 0.1f, 1);
+        Gdx.gl.glClearColor(1f, 1f, 1f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        batch.setProjectionMatrix(camera.combined);
-        bgRenderer.begin(ShapeRenderer.ShapeType.Filled);
-        bgRenderer.setColor(255, 255, 255, 1);
-        bgRenderer.rect(0, 0, width, height);
-        bgRenderer.end();
 
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
@@ -80,6 +82,10 @@ public class SplashScreen implements Screen
             libgdxSplashSprite.draw(batch);
         }
         else if(screenToDisplay == 1)
+        {
+            afLogoSprite.draw(batch);
+        }
+        else if(screenToDisplay == 2)
         {
             gameSplashSprite.draw(batch);
         }
@@ -126,9 +132,11 @@ public class SplashScreen implements Screen
     public void dispose()
     {
         batch.dispose();
-        libgdxSplashTexture.dispose();
-        gameSplashTexture.dispose();
-        bgRenderer.dispose();
+        libgdxSplashSprite.getTexture().dispose();
+        gameSplashSprite.getTexture().dispose();
+        batch = null;
+        libgdxSplashSprite = null;
+        gameSplashSprite = null;
     }
 
 }
