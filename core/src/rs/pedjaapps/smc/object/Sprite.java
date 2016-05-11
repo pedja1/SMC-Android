2b8a6182919804aab3ac190cd325ba6b2cb9680f
Predrag Cokulov <predragcokulov@gmail.com>
2016-05-11 21:05:41 +0200

    added missing sublevels

--- core/src/rs/pedjaapps/smc/object/Sprite.java
+++ core/src/rs/pedjaapps/smc/object/Sprite.java
@@ -4,7 +4,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
@@ -65,12 +64,6 @@ public class Sprite extends GameObject
             spriteBatch.draw(region, mOrigDrawRect.x, mOrigDrawRect.y, originX, originY, width, mOrigDrawRect.height, 1, 1, rotation);
             region.flip(flipX, flipY);//return it to original
         }
-
-        //debug
-        //mRotationZ = mRotationZ + 0.1f;
-        //if(mRotationZ > 360)mRotationZ = 0;
-        //applyRotation();
-        //debug end
     }
 
     @Override
@@ -82,7 +75,6 @@ public class Sprite extends GameObject
     @Override
     public void initAssets()
     {
-        //mRotationX = mRotationY = mRotationZ = 0;
         if (mRotationZ == 90 && mRotationX == 0 && mRotationY == 0)
         {
             mRotationY = 180;
@@ -106,7 +98,6 @@ public class Sprite extends GameObject
 
         if (txt == null && region == null)
         {
-            System.out.println(this);
             throw new GdxRuntimeException("both Texture and TextureRegion are null");
         }
 
@@ -138,59 +129,6 @@ public class Sprite extends GameObject
             mColRect.x = mDrawRect.x + ((mDrawRect.x + mDrawRect.width) - (mColRect.x + mColRect.width));
         }
 
-        /*if (mRotationZ == 90.0f)
-        {
-            // rotate position
-            Vector2 pos = rotate(mDrawRect, mDrawRect.width * 0.5f, mDrawRect.height * 0.5f, mRotationZ);
-            mDrawRect.x = pos.x;
-            mDrawRect.y = pos.y - mDrawRect.width;
-
-            // rotate collision position
-            Vector2 colPos = rotate(mColRect, mColRect.width * 0.5f, mColRect.height * 0.5f, mRotationZ);
-            mColRect.x = colPos.x;
-            mColRect.y = colPos.y - mColRect.width;
-
-            // switch width and height
-            float orig_w = mDrawRect.width;
-            mDrawRect.width = mDrawRect.height;
-            mDrawRect.height = orig_w;
-            // switch collision width and height
-            float orig_col_w = mColRect.width;
-            mColRect.width = mColRect.height;
-            mColRect.height = orig_col_w;
-            World.VECTOR2_POOL.free(pos);
-            World.VECTOR2_POOL.free(colPos);
-        }
-        // mirror
-        else if (mRotationZ == 180.0f)
-        {
-            mColRect.x = mDrawRect.width - (mColRect.width + mColRect.x);
-            mColRect.y = mDrawRect.height - (mColRect.height + mColRect.y);
-        }
-        else if (mRotationZ == 270.0f)
-        {
-            // rotate position
-            Vector2 pos = rotate(mDrawRect, mDrawRect.width * 0.5f, mDrawRect.height * 0.5f, mRotationZ);
-            mDrawRect.x = pos.x;
-            mDrawRect.y = pos.y;
-            // rotate collision position
-            Vector2 colPos = rotate(mColRect, mColRect.width * 0.5f, mColRect.height * 0.5f, mRotationZ);
-            mColRect.x = colPos.x;
-            mColRect.y = colPos.y;
-
-            // switch width and height
-            float orig_w = mDrawRect.width;
-            mDrawRect.width = mDrawRect.height;
-            mDrawRect.height = orig_w;
-            // switch collision width and height
-            float orig_col_w = mColRect.width;
-            mColRect.width = mColRect.height;
-            mColRect.height = orig_col_w;
-            World.VECTOR2_POOL.free(pos);
-            World.VECTOR2_POOL.free(colPos);
-        }
-        position.x = mColRect.x;
-        position.y = mColRect.y;*/
         if (mRotationZ != 0)
         {
             float originY = getOriginY();
@@ -214,23 +152,16 @@ public class Sprite extends GameObject
     /**
      * @param originX , originY, rotation point relative to self
      */
-    public void rotate2(Rectangle sourceRect, Rectangle destRect, float originX, float originY, float rotate)
+    private void rotate2(Rectangle sourceRect, Rectangle destRect, float originX, float originY, float rotate)
     {
         float x = sourceRect.x;
         float y = sourceRect.y;
         float centerX = sourceRect.x + originX;
         float centerY = sourceRect.y + originY;
-        float hW = sourceRect.width / 2;
-        float hH = sourceRect.height / 2;
         float w = sourceRect.width;
         float h = sourceRect.height;
 
         Polygon polygon = new Polygon(new float[]{
-                /*centerX - hW, centerY - hH,
-                centerX - hW, centerY + hH,
-                centerX + hW, centerY + hH,
-                centerX + hW, centerY - hH*/
-
                 x, y,
                 x, y + h,
                 x + w, y + h,
@@ -240,32 +171,6 @@ public class Sprite extends GameObject
         polygon.setOrigin(centerX, centerY);
         polygon.setRotation(rotate);
         destRect.set(polygon.getBoundingRectangle());
-    }
-
-    public Vector2 rotate(Rectangle rect, float originX, float originY, float rotation)
-    {
-        /*
-        degree = 90
-        x = 4
-        y = 4
-        width = 2
-        height = 1
-        ox = width / 2			//relative center x
-        oy = height / 2         //relative center y
-        cx = x + ox		//world center x
-        cy = y + ox		//world center y
-
-        rx = cx + (ox * cos(degree)) - (oy * sin(degree))
-        ry = cy + (ox * sin(degree)) + (oy * cos(degree))
-        */
-        Vector2 r = World.VECTOR2_POOL.obtain();
-
-        float cx = rect.x + originX;
-        float cy = rect.y + originY;
-
-        r.x = cx + (originX * MathUtils.cosDeg(rotation)) - (originY * MathUtils.sinDeg(rotation));
-        r.y = cy + (originX * MathUtils.sinDeg(rotation)) + (originY * MathUtils.cosDeg(rotation));
-        return r;
     }
 
     /**
