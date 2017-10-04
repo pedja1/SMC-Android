9c5c5bc3f84a033dd6b077636eba6a4fd4c37319
Benjamin Schulte <bs@golfgl.de>
2017-10-04 20:04:26 +0200

    lwjgl2

--- desktop/src/rs/pedjaapps/smc/desktop/DesktopLauncher.java
+++ desktop/src/rs/pedjaapps/smc/desktop/DesktopLauncher.java
@@ -1,32 +1,30 @@
 package rs.pedjaapps.smc.desktop;
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
 import rs.pedjaapps.smc.MaryoGame;
 
-public class DesktopLauncher
-{
-    public static void main(String[] arg)
-    {
+public class DesktopLauncher {
+    public static void main(String[] arg) {
         //BasicConfigurator.configure();
-        /*LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 1024;
-        config.height = 576;
-        config.resizable = false;
-        config.allowSoftwareMode = false;
-        //config.vSyncEnabled = false;
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = MaryoGame.NATIVE_WIDTH;
+        config.height = MaryoGame.NATIVE_HEIGHT;
+        //config.resizable = false;
+        //config.allowSoftwareMode = false;
+        config.vSyncEnabled = false;
         //config.foregroundFPS = 0;
         //config.backgroundFPS = 0;
         //config.addIcon("data/icons/desktop_mac.png", Files.FileType.Internal);
         //config.addIcon("data/icons/desktop_win_lin.png", Files.FileType.Internal);
         //config.addIcon("data/icons/desktop_win.png", Files.FileType.Internal);
-        new LwjglApplication(new MaryoGame(), config);*/
+        new LwjglApplication(new MaryoGame(null), config);
 
-        Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setWindowedMode(MaryoGame.NATIVE_WIDTH, MaryoGame.NATIVE_HEIGHT);
+        //Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        //config.setWindowedMode(MaryoGame.NATIVE_WIDTH, MaryoGame.NATIVE_HEIGHT);
         //config.setResizable(false);
-        config.useVsync(false);
-        new Lwjgl3Application(new MaryoGame(null), config);
+        //config.useVsync(false);
+        //new Lwjgl3Application(new MaryoGame(null), config);
     }
 }
