0039d80c485dc6db7b5552f825a46cdba5e67209
Benjamin Schulte <bs@golfgl.de>
2017-11-04 17:48:08 +0100

    preload logo

--- html/src/rs/pedjaapps/smc/client/HtmlLauncher.java
+++ html/src/rs/pedjaapps/smc/client/HtmlLauncher.java
@@ -4,9 +4,17 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.badlogic.gdx.backends.gwt.preloader.Preloader;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style;
 import com.google.gwt.event.logical.shared.ResizeEvent;
 import com.google.gwt.event.logical.shared.ResizeHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.InlineHTML;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 import rs.pedjaapps.smc.MaryoGame;
 
@@ -30,6 +38,37 @@ public class HtmlLauncher extends GwtApplication {
     @Override
     public ApplicationListener createApplicationListener() {
         return new MaryoGame(null);
+    }
+
+    @Override
+    public Preloader.PreloaderCallback getPreloaderCallback() {
+        final Panel preloaderPanel = new VerticalPanel();
+        preloaderPanel.setStyleName("gdx-preloader");
+        final Image logo = new Image(GWT.getHostPageBaseURL() + "logo_preload.png");
+        logo.setStyleName("preloadlogo");
+        preloaderPanel.add(logo);
+        final Panel meterPanel = new SimplePanel();
+        meterPanel.setStyleName("gdx-meter");
+        meterPanel.addStyleName("nostripes ");
+        final InlineHTML meter = new InlineHTML();
+        final Style meterStyle = meter.getElement().getStyle();
+        meterStyle.setWidth(0, Style.Unit.PCT);
+        meterPanel.add(meter);
+        preloaderPanel.add(meterPanel);
+        getRootPanel().add(preloaderPanel);
+        return new Preloader.PreloaderCallback() {
+
+            @Override
+            public void error(String file) {
+                System.out.println("error: " + file);
+            }
+
+            @Override
+            public void update(Preloader.PreloaderState state) {
+                meterStyle.setWidth(100f * state.getProgress(), Style.Unit.PCT);
+            }
+
+        };
     }
 
     class ResizeListner implements ResizeHandler {
