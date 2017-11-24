5286ca6ec4760e56b366219d2040fd80ce2d484d
Benjamin Schulte <bs@golfgl.de>
2017-11-24 22:04:55 +0100

    Mock for Gpgs Service in Desktop Project

--- desktop/src/rs/pedjaapps/smc/desktop/DesktopLauncher.java
+++ desktop/src/rs/pedjaapps/smc/desktop/DesktopLauncher.java
@@ -2,7 +2,12 @@ package rs.pedjaapps.smc.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.utils.Array;
 
+import de.golfgl.gdxgamesvcs.MockGameServiceClient;
+import de.golfgl.gdxgamesvcs.NoGameServiceClient;
+import de.golfgl.gdxgamesvcs.achievement.IAchievement;
+import de.golfgl.gdxgamesvcs.leaderboard.ILeaderBoardEntry;
 import rs.pedjaapps.smc.MaryoGame;
 
 public class DesktopLauncher {
@@ -19,7 +24,34 @@ public class DesktopLauncher {
         //config.addIcon("data/icons/desktop_mac.png", Files.FileType.Internal);
         //config.addIcon("data/icons/desktop_win_lin.png", Files.FileType.Internal);
         //config.addIcon("data/icons/desktop_win.png", Files.FileType.Internal);
-        new LwjglApplication(new MaryoGame(null), config);
+        MaryoGame listener = new MaryoGame(null);
+        listener.gpgsClient = new MockGameServiceClient(.8f) {
+            @Override
+            protected Array<ILeaderBoardEntry> getLeaderboardEntries() {
+                return null;
+            }
+
+            @Override
+            protected Array<String> getGameStates() {
+                return null;
+            }
+
+            @Override
+            protected byte[] getGameState() {
+                return new byte[0];
+            }
+
+            @Override
+            protected Array<IAchievement> getAchievements() {
+                return null;
+            }
+
+            @Override
+            protected String getPlayerName() {
+                return "testnamevolllangundso";
+            }
+        };
+        new LwjglApplication(listener, config);
 
         //Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
         //config.setWindowedMode(MaryoGame.NATIVE_WIDTH, MaryoGame.NATIVE_HEIGHT);
