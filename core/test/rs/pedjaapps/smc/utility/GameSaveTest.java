0ebe5292b38b8d93a1c947589f97a4aab7685983
pedja1 <predragcokulov@gmail.com>
2019-03-21 21:01:27 +0100

    remove unused imports

--- core/test/rs/pedjaapps/smc/utility/GameSaveTest.java
+++ core/test/rs/pedjaapps/smc/utility/GameSaveTest.java
@@ -1,11 +1,9 @@
 package rs.pedjaapps.smc.utility;
 
-import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
-import com.badlogic.gdx.backends.headless.HeadlessPreferences;
 import com.badlogic.gdx.graphics.GL20;
 
 import org.junit.BeforeClass;
@@ -14,7 +12,7 @@ import org.mockito.Mockito;
 
 import rs.pedjaapps.smc.object.maryo.Maryo;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by Benjamin Schulte on 31.10.2017.
@@ -25,12 +23,29 @@ public class GameSaveTest {
     public static void init() {
         // Note that we don't need to implement any of the listener's methods
         Gdx.app = new HeadlessApplication(new ApplicationListener() {
-            @Override public void create() {}
-            @Override public void resize(int width, int height) {}
-            @Override public void render() {}
-            @Override public void pause() {}
-            @Override public void resume() {}
-            @Override public void dispose() {}
+            @Override
+            public void create() {
+            }
+
+            @Override
+            public void resize(int width, int height) {
+            }
+
+            @Override
+            public void render() {
+            }
+
+            @Override
+            public void pause() {
+            }
+
+            @Override
+            public void resume() {
+            }
+
+            @Override
+            public void dispose() {
+            }
         });
 
         // Use Mockito to mock the OpenGL methods since we are running headlessly
